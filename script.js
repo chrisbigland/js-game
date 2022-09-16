819f2e553784e75a7cfaaa707dc5738fe32edcb7
CHRIS BIGLAND <christopherbigland@gmail.com>
2022-09-16 10:37:43 +0100

    bouncing block example code complete. Block bounces from all of the walls

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ let canvas, context;
 canvas = document.querySelector("#canvas")
 context = canvas.getContext("2d");
 
+// the context is what we use to draw on the canvas element
+
 
 // canvas.addEventListener("mouseover", () => {
 //     canvas.style.cursor = "col-resize";
@@ -28,12 +30,12 @@ yv = Math.floor(Math.random() * 101 + 25) / FPS;
 
 
 // random ball direction
-if (Math.floor(Math.random() * 2) == 0) {      // if this random number between 0 and 1, multiplied by 2 and then rounded down to the nearest integer - run the function (like a coin toss!)
+if (Math.floor(Math.random() * 2) == 0) {       // if this random number between 0 and 1, multiplied by 2 and then rounded down to the nearest integer (will either be 1 or 0) is equal to 0 - run the function (like a coin toss!)
     xv = -xv;   // -xv means it will be going in the other direction
 
 }
 
-if (Math.floor(Math.random() * 2) == 0) {      // if this random number between 0 and 1, multiplied by 2 and then rounded down to the nearest integer - run the function (like a coin toss!)
+if (Math.floor(Math.random() * 2) == 0) {     
     yv = -yv;
 
 }
@@ -44,14 +46,26 @@ function update() {
     by += yv;
 
 
-    // bounce the ball off each wall
-    if (bx - bs / 2 < 0 && xv < 0) {                        // if the ball x value minus the ball size divided by 2 (I imagine because ) is less than 1 and the velocity is less than 0, the velocity will be set at a minus 
-        xv = -xv;
+    // bounce the ball off each wall - put in conditions to detect if ball is near the wall
+    if (bx - bs / 2 < 0 && xv < 0) {                        // if the ball x value minus half of the ball size is less than 0 (going off the left of the screen) and the velocity is less than 0 (means it's travelling towards the left), the velocity will be set at a minus (making it go other way in x direction) 
+        xv = -xv; // want to switch the x velocity to a minus so that it will go the other way in the x direction. 
+    }
+
+    if (bx + bs / 2 > canvas.width && xv > 0) {            // if the ball x value PLUS half of the ball size is MORE than the CANVAS WIDTH and the velocity MORE than 0(means it is travelling in a POSITIVE direction), the X velocity will be set at a minus. 
+        xv = -xv; // want to switch the x velocity to a minus so that it will go the other way in the x direction. 
+    }
+
+    if (by - bs / 2 < 0 && yv < 0) {            // if the ball y value minus half of the ball size is less than 0 and the velocity less than 0, the velocity will be set at a minus. 
+        yv = -yv; // want to switch the y velocity to a minus so that it will go the other way in the y direction. 
+    }
+
+    if (by + bs / 2 > canvas.height && yv > 0) {            // if the ball y value PLUS half of the ball size is MORE than the CANVAS HEIGHT (travelling downwards) and the velocity (yv) MORE than 0, the Y velocity will be set at a minus. 
+        yv = -yv; // want to switch the x velocity to a minus so that it will go the other way in the x direction. 
     }
 
     // draw background and ball
-    context.fillStyle = "black";
-    context.fillRect(0, 0, canvas.width, canvas.height);
+    context.fillStyle = "black"; // fillStyle needs to come BEFORE fillRect. 
+    context.fillRect(0, 0, canvas.width, canvas.height);   // fillRect - first two values are x and y positions, second two are width and height measurements. 
     context.fillStyle = "yellow";
     context.fillRect(bx - bs / 2, by - bs / 2, bs, bs); // bx is wherever the ball is. We minus the ball
 }
