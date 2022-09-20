ce2b24f084ab3b7f5401bfc60ff7b4ee482df131
CHRIS BIGLAND <christopherbigland@gmail.com>
2022-09-20 14:12:37 +0100

    indv. ids added to en cards

--- script.js
+++ script.js
@@ -1,29 +1,49 @@
 const enCardContainer = document.querySelector("#en-card-container");
 const ptCardContainer = document.querySelector("#pt-card-container");
-const enCard = document.querySelector(".en-card");
-const enCardContent = document.querySelector("#en-card-content");
 
 
+// let userChoicesArr = []; // limit to two items
+// let matchedChoices = [];
 
 
 //EN CARD CLASS
 class EnNounCard {
-    constructor(word, picture){
+    constructor(word, picture, id){
     this.word = word,
     this.picture = picture
+    this.id = id;
+        // this.audio = audio
     }
 
+    // below - change 'id' to 'data-id' once I've made the cards visible. 
 createEnLanguageCard() {
     const languageCard = `
-    <div class="en-card"><div id="en-card-content"><h2>${this.word}</h2> <img id="enImg" src="${this.picture}"></div></div>` 
+    <div class="en-card" id="british-${this.id}"><div id="en-card-content"><h2>${this.word}</h2> <img id="enImg" src="${this.picture}"></div></div>` 
     return languageCard;
 }
 
+
+
 }
 
-// enCard.addEventListener( "click", (e) => {
-//     e.target.classList.remove("en-card-content")
+
+
+
+// const british-4 = document.querySelector = ("#british-4");
+// const britishID = document.querySelector(`british-${this.id}`)
+
+
+
+// britishID.addEventListener("click", (card) => {
+//     card.style.visibility = "visible";
 // })
+
+// enCard.addEventListener("click", () => {
+//     // e.target.classList.remove("en-card-content")
+//     // enCardContent.style.visibility = "visible";
+//     console.log(`this click event works`)
+// })
+
 
 // PT CARD CLASS
 class PtNounCard {
@@ -32,6 +52,10 @@ class PtNounCard {
     this.picture = picture,
     this.hint = hint
 }
+
+// playAudio() {
+//     // grab the audio tag -> then hit play/pause -> look into audio/video tag W3Schools - add in the following 'onclick' to the 'help' button below. <button onclick=${this.playAudio()}>
+// }
 
 createPtLanguageCard() {
     const languageCard = `
@@ -43,25 +67,23 @@ createPtLanguageCard() {
 
 }
 
-
-
 // CARD DATA stored as variables
-const enDog = new EnNounCard ("dog", "./images/dog.jpeg");
-const enCat = new EnNounCard ("cat", "./images/cat.webp");
-const enRabbit = new EnNounCard ("rabbit", "./images/rabbit.jpeg");
-const enChicken = new EnNounCard ("chicken", "./images/chicken.jpeg");
-const enPig = new EnNounCard ("pig", "./images/pig.jpeg");
-const enCow = new EnNounCard ("cow", "./images/cow.jpeg");
-const enSheep = new EnNounCard ("sheep", "./images/sheep.jpeg");
-const enHorse = new EnNounCard ("horse", "./images/horse.jpeg");
-const enLion = new EnNounCard ("lion", "./images/lion.jpeg");
-const enElephant = new EnNounCard ("elephant", "./images/elephant.jpeg");
-const enMonkey = new EnNounCard ("monkey", "./images/monkey.jpeg");
-const enBear = new EnNounCard ("bear", "./images/bear.jpeg");
-const enMouse = new EnNounCard ("mouse", "./images/mouse.jpeg");
-const enFrog = new EnNounCard ("frog", "./images/frog.jpeg");
-const enWolf = new EnNounCard ("wolf", "./images/wolf.jpeg");
-const enTiger = new EnNounCard ("tiger", "./images/tiger.jpeg");
+const enDog = new EnNounCard ("dog", "./images/dog.jpeg", 1);
+const enCat = new EnNounCard ("cat", "./images/cat.webp", 2);
+const enRabbit = new EnNounCard ("rabbit", "./images/rabbit.jpeg", 3);
+const enChicken = new EnNounCard ("chicken", "./images/chicken.jpeg", 4);
+const enPig = new EnNounCard ("pig", "./images/pig.jpeg", 5);
+const enCow = new EnNounCard ("cow", "./images/cow.jpeg", 6);
+const enSheep = new EnNounCard ("sheep", "./images/sheep.jpeg", 7);
+const enHorse = new EnNounCard ("horse", "./images/horse.jpeg", 8);
+const enLion = new EnNounCard ("lion", "./images/lion.jpeg", 9);
+const enElephant = new EnNounCard ("elephant", "./images/elephant.jpeg", 10);
+const enMonkey = new EnNounCard ("monkey", "./images/monkey.jpeg", 11);
+const enBear = new EnNounCard ("bear", "./images/bear.jpeg", 12);
+const enMouse = new EnNounCard ("mouse", "./images/mouse.jpeg", 13);
+const enFrog = new EnNounCard ("frog", "./images/frog.jpeg", 14);
+const enWolf = new EnNounCard ("wolf", "./images/wolf.jpeg", 15);
+const enTiger = new EnNounCard ("tiger", "./images/tiger.jpeg", 16);
 
 const ptDog = new PtNounCard ("cachorro (m)", "./images/dog.jpeg")
 const ptCat = new PtNounCard ("gato (m)", "./images/cat.webp")
@@ -80,12 +102,39 @@ const ptFrog = new PtNounCard ("sapo (m)", "./images/frog.jpeg");
 const ptWolf = new PtNounCard ("lobo (m)", "./images/wolf.jpeg");
 const ptTiger = new PtNounCard ("tigre (m)", "./images/tiger.jpeg");
 
+// Array.forEeach(card => {
+//     let enCard = new card(card.animal, card.imgSrc, card.audio)
+// })
+
 // EN CARD ARRAY
 const enCardArr = [enDog, enCat, enRabbit, enChicken, enPig, enCow, enSheep, enHorse, enLion, enElephant, enMonkey, enBear, enMouse, enFrog, enWolf, enTiger]
 
 enCardArr.forEach((card) => {
     enCardContainer.innerHTML += card.createEnLanguageCard();
+   
 })
+const enCard = document.querySelectorAll(".en-card");
+console.log(enCard);
+
+
+const enCardContent = document.querySelector("#en-card-content");
+
+let enCardID;   
+enCard.forEach((card) => {                  // looping through the enCard node list to add the click event listener
+    card.addEventListener("click", (e) => {
+        console.log(e.target.id)
+        // enCardID = e.target.id
+        // console.log(enCardID)
+        // // const enCardIDVar = document.querySelector(`#${enCardID}`)
+        // enCardID.classList.add("unhide")
+    })
+})
+
+
+// const buttonClicked = () => {
+//     alert("button clicked, id"+this.id+" text"+this.innerHTML);
+// }
+
 
 //TIMER
 //setinterval function
@@ -103,24 +152,28 @@ const newGameBtn = document.querySelector("#new-game-btn");
 
 newGameBtn.addEventListener("click", () => {
     setInterval(function myTimer() {
-    // seconds--
-    // timerSeconds.innerHTML = seconds;
-    if (seconds === 0) {
-        minutes = minutes - 1
+    // newGameBtn.addEventListener("click", () => {
+    //     // seconds = 0;
+    //     // minutes = 4;
+    //     clearInterval(myTimer)
+    //     myTimer()
+    // })
+    if (seconds === 0) {            // reducing the minutes by 1 when seconds gets to 0. 
+        minutes = minutes - 1  // not 100% sure why this can't be minutes--?
         timerMins.innerHTML = `${minutes}`
-        seconds = 60
+        seconds = 60            //replenishing seconds to 60
     }
-    seconds--
+    seconds--                   // seconds countdown by x1 every time function run (every second)
     timerSeconds.innerHTML = seconds;
     if (seconds < 10) {
-        zeroSecond.innerHTML = `0`;
+        zeroSecond.innerHTML = "0";
     }
-    // if (seconds >= 10) {
-    //     zeroSecond.remove();
-    // } adding this seems to remove the '0' before single digit numbers but if I remove it then a '0' is added to double digit numbers
-    if (seconds <= 0 && minutes === 0) {
-        clearInterval(myTimer)
-        // alert(`Times Up!`)
+    if (seconds >= 10) {
+        zeroSecond.innerHTML = "";
+    }
+    if (seconds === 0 && minutes === 0) {
+        clearInterval(myTimer)      // stops timer when secs and mins on 0
+        alert("Times Up!")
     }
 }, 1000) // setInterval repeats a function at every given time-interval. first parameter is the function, second is the time in milliseconds. 
 })
@@ -136,14 +189,18 @@ ptCardArr.forEach((card) => {
 
 
 
+// enCard.addEventListener("click", () => {
+//     // e.target.classList.remove("en-card-content")
+//     // enCardContent.style.visibility = "visible";
+//     console.log(`this click event works`)
+// })
+
+
 // OUTSTANDING ACTIONS
-// make cards same size 
-// add shuffle / new game button
-// add 'I found a pair' button
-//create timer
-// create back of card design
+// amend timer - fix minus issue once alert is cleared. 
+// create click event for cards
 // make cards flip
 // add audio files - work out how to target each card as they've been created em masse using a function. Could we add the button into each PtNounCard instead?
 // make design responsive
-// make cards shuffle
+// make cards shuffle - use math.random to create a random number and when 'new game' button is clicked, the cards e.g. ptDog enDog are assigned new indexes in the array. 
 
